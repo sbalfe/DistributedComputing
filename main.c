6bb8f9e980b65a888428549cccfc12849c05544d
simon balfe <sbmain17@gmail.com>
2022-12-19 21:33:21 +0000

    update

--- main.c
+++ main.c
@@ -24,15 +24,15 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &n_processors);
 
-    int *p_buffer;
+    int *p_buffer = NULL;
 
-//    if (my_rank == 0){
-//        p_buffer = malloc(sizeof(int) * 2);
-//        //p_buffer[0] = 1;
-//        //p_buffer[1] = 2;
-//    }
+    if (my_rank == 0){
+        p_buffer = malloc(sizeof(int) * 2);
+        p_buffer[0] = 1;
+        p_buffer[1] = 2;
+    }
 
-   // MPI_Bcast(p_buffer, 2, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(p_buffer, 2, MPI_INT, 0, MPI_COMM_WORLD);
 
     printf("%d: hey\n", my_rank);
 
