c0d68677cb7c2b433ce4fa3ed9b9d869c4278c0b
simon balfe <sbmain17@gmail.com>
2022-12-20 23:12:20 +0000

    update

--- main.c
+++ main.c
@@ -1,42 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <mpi.h>
 
-// test
-typedef struct context context;
+struct Context {
+    uint array_size;
+    int n_processors;
+    int rank;
+    double precision;
+    uint *block_size;
+    double *local_buffer;
+} typedef context_t;
 
-struct context {
-    context *p_next;
-};
+
+void print_array(void* arr, uint arrSize) {
+
+    double *array = (double *) arr;
+    for (uint y = 0; y < arrSize; ++y) {
+
+        for (int x = 0; x < arrSize; ++x) {
+            printf("%f ", array[arrSize * y + x]);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char **argv) {
 
-    int rc, my_rank, n_processors, name_len;
+    int rc = MPI_Init(&argc, &argv);
+    int name_len;
+
+    context_t context;
+    context.array_size = (int) strtol(argv[1], 0,10);
+    context.precision = strtof(argv[2], 0);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &context.rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &context.n_processors);
 
     char name[MPI_MAX_PROCESSOR_NAME];
-
-    rc = MPI_Init(&argc, &argv);
 
     if (rc != MPI_SUCCESS) {
         printf("Error starting MPI test program\n");
         MPI_Abort(MPI_COMM_WORLD, rc);
     }
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &n_processors);
+    double *input_buffer = malloc(sizeof(double) * ((size_t) pow(context.array_size,2)));
+    context.block_size = malloc(sizeof(double) * context.n_processors);
 
-    int *send_buffer = calloc(64, sizeof(int));
-    int *receive_value = calloc(4,sizeof(int));
+    uint operations = (uint) (pow(context.array_size,2)) / context.n_processors;
+    uint remainder = (uint) (pow(context.array_size,2)) % context.n_processors;
 
-    MPI_Scatter(send_buffer, 4 , MPI_INT, receive_value, 4, MPI_INT, 1, MPI_COMM_WORLD);
+    if (context.rank == 0) {
+        for (uint i = 0; i < context.n_processors; ++i) {
+            if (remainder != 0) {
+                context.block_size[i] = operations + 1;
+                remainder--;
+            } else {
+                context.block_size[i] = operations;
+            }
+        }
+    }
 
-    printf("%d: hey the second value is %d\n", my_rank, receive_value[0]);
+    MPI_Bcast(context.block_size, context.n_processors, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
 
-    name_len = MPI_MAX_PROCESSOR_NAME;
-    MPI_Get_processor_name(name, &name_len);
+    context.local_buffer = malloc(sizeof(double) * context.block_size[context.rank]);
 
+    // make one processor allocate the array.
+    if (context.rank == 0) {
+        for (int y = 0; y < context.array_size; ++y) {
+            if (y == 0) {
+                for (int x = 0; x < context.array_size; ++x) {
+                    input_buffer[context.array_size * y + x] = 1;
+                }
+            } else {
+                for (int x = 0; x < context.array_size; ++x) {
+                    if (x == 0) {
+                        input_buffer[context.array_size * y + x] = 1;
+                    } else {
+                        input_buffer[context.array_size * y + x] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    MPI_Scatter(input_buffer, (int) pow(context.array_size,2) , MPI_DOUBLE,
+                context.local_buffer , (int) context.block_size[context.rank], MPI_DOUBLE,
+                0, MPI_COMM_WORLD);
+
+    print_array(context.local_buffer, context.array_size);
     MPI_Finalize();
-
     return 0;
 }
