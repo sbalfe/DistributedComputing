79d01da03cd9adcf292f72dbd348ca9699703b44
Simon Balfe <sbmain17@gmail.com>
2022-12-22 16:33:59 +0000

    message

---
+++ cmake-build-debug/CMakeFiles/DistributedComputing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DistributedComputing.dir/main.c.o"
+  "CMakeFiles/DistributedComputing.dir/main.c.o.d"
+  "DistributedComputing"
+  "DistributedComputing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DistributedComputing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/shriller44/university/University/ParallelComputing-CM30225/Coursework/DistributedComputing
+# Install script for directory: /home/shriller44/university/DistributedComputing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/shriller44/university/University/ParallelComputing-CM30225/Coursework/DistributedComputing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/shriller44/university/DistributedComputing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
