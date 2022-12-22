1ce930314200c3ec6e4698a231454299f93775c5
Simon Balfe <sbmain17@gmail.com>
2022-12-22 20:07:18 +0000

    update

--- main.c
+++ main.c
@@ -126,7 +126,7 @@ int main(int argc, char **argv) {
                     if (x == 0) {
                         context->input_buffer[context->array_size * y + x] = 1;
                     } else {
-                        context->input_buffer[context->array_size * y + x] = 0;
+                        context->input_buffer[context->array_size * y + x] = 2;
                     }
                 }
             }
