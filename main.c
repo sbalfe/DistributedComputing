b7a83d7d14bff3a0e9b4a8222613be273838f16b
Simon Balfe <sbmain17@gmail.com>
2022-12-23 16:57:03 +0000

    update

--- main.c
+++ main.c
@@ -77,9 +77,6 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &context->rank);
     MPI_Comm_size(MPI_COMM_WORLD, &context->n_processors);
 
-    //printf("my rank is %d\n", context->rank);
-    char name[MPI_MAX_PROCESSOR_NAME];
-
     context->block_size = malloc((ssize_t) sizeof(double) * context->n_processors);
     context->displacements = malloc((ssize_t) sizeof(double) * context->n_processors);
     context->input_buffer = malloc((ssize_t) sizeof(double) * ((ssize_t) pow(context->array_size,2)));
