6b0de4e07b5403e06fe7f7e877761047bb0bb65e
Simon Balfe <sbmain17@gmail.com>
2022-12-23 16:49:45 +0000

    update

--- main.c
+++ main.c
@@ -1,31 +1,3 @@
-//#include <mpi.h>
-//#include <stdio.h>
-//
-//int main(int argc, char** argv) {
-//  // Initialize the MPI environment
-//  MPI_Init(NULL, NULL);
-//
-//  // Get the number of processes
-//  int world_size;
-//  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-//
-//  // Get the rank of the process
-//  int world_rank;
-//  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-//
-//  // Get the name of the processor
-//  char processor_name[MPI_MAX_PROCESSOR_NAME];
-//  int name_len;
-//  MPI_Get_processor_name(processor_name, &name_len);
-//
-//  // Print off a hello world message
-//  printf("Hello world from processor %s, rank %d out of %d processors\n",
-//         processor_name, world_rank, world_size);
-//
-//  // Finalize the MPI environment.
-//  MPI_Finalize();
-//}
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -40,7 +12,6 @@ struct Context {
     int *displacements;
     double *local_buffer;
     double *input_buffer;
-    double *output_buffer;
     int complete;
 } typedef context_t;
 
@@ -71,7 +42,7 @@ void array_relaxation(context_t *context){
 
     for (int i = 0; i < context->block_size[context->rank] ; ++i){
 
-        // figure out what position we are at in the array in terms of rows and columns
+        // figure out what position we are at in the initial input buffer.
         int y = (array_offset + i) / (int) context->array_size;
         int x = (array_offset + i) % (int) context->array_size;
 
