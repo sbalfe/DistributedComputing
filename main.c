042d159fd851e7685c0173126f47604682d8bc83
Simon Balfe <sbmain17@gmail.com>
2022-12-23 13:58:14 +0000

    update

--- main.c
+++ main.c
@@ -196,6 +196,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    printf("rank: %d", context->rank);
     if (context->rank == 0) {
         print_array(context->input_buffer, context->array_size);
     }
